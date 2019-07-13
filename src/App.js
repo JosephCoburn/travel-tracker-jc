f07add6400211558f3c0c97e888ae6aedb0431d8
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-07-13 16:07:44 -0700

    refactor index.js and App.js (x.6,23-30,37,53)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
@@ -7,21 +7,39 @@ import Navbar from "./components/navbar.component"
 import VoyagesList from "./components/voyages-list.component";
 import EditVoyage from "./components/edit-voyage.component";
 import CreateVoyage from "./components/create-voyage.component";
- 
+import LoginPage from "./pages/LoginPage/LoginPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
+class App extends Component {
+  constructor() {
+    super();
+  }
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <br/>
-        <Route path="/" exact component={VoyagesList} />
-        <Route path="/edit/:id" component={EditVoyage} />
-        <Route path="/create" component={CreateVoyage} />
-      </div>
-    </Router>
-  );
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Navbar />
+          <br/>
+          <Route path="/" exact component={VoyagesList} />
+          <Route path="/edit/:id" component={EditVoyage} />
+          <Route path="/create" component={CreateVoyage} />
+          <Route exact path='/signup' render={({ history }) => 
+          <SignupPage
+            history={history}
+            handleSignupOrLogin={this.handleSignupOrLogin}
+          />
+        }/>
+          <Route exact path='/login' render={({ history }) => 
+            <LoginPage
+              history={history}
+              handleSignupOrLogin={this.handleSignupOrLogin}
+            />
+          }/>
+        </div>
+      </Router>
+    );
+  }
 }
  
 export default App;
