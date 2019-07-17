ef41ac9b2d7fcf3d758dcba01577af68ba781734
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-07-16 19:50:43 -0700

    her oku

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 require('dotenv').config();
 
 const app = express();
@@ -9,7 +20,6 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
