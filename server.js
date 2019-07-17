bf6c36cb5646f4f3b534e671233e0857beac9aa7
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-07-16 17:29:22 -0700

    fix heroku bug

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 require('dotenv').config();
 
 const app = express();
