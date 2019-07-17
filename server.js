c388d0ae95881fa532b53a8ceb373d7a11ef6197
Joe Coburn <joecoburn@Joes-MacBook-Pro.local>
2019-07-16 17:38:31 -0700

    fix heroku bug

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 require('dotenv').config();
 
 const app = express();
@@ -20,6 +9,14 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+var history = require('connect-history-api-fallback');
+
+var connect = require('connect');
+
+var app = connect()
+  .use(history())
+  .listen(3000);
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
